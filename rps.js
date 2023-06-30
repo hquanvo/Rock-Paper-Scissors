c4c91a3e4f14864bb65be2949f32038c2d192b54
hquanvo <vo.hoang.quan2003@gmail.com>
2023-06-30 18:25:25 +0700

    Add ability to play game

--- rps.js
+++ rps.js
@@ -6,8 +6,6 @@ let cpuChoice;
 let cpuScore;
 let playerChoice;
 let playerScore;
-let losingMessage = "You lose!";
-let winningMessage = "You win!";
 
 function getComputerChoice() {
     switch((Math.floor(Math.random() * 10)) % 3) {
@@ -28,50 +26,69 @@ function getPlayerChoice() {
 }
 
 function playRound(playerChoice, cpuChoice) {
-    if (playerChoice === cpuChoice) return "It's a draw!";
+    if (playerChoice === cpuChoice) return "It's a tie!";
     else if ((playerChoice === "rock" && cpuChoice === "paper") || 
-    (playerChoice === "paper" && cpuChoice === "scissors") || 
-    (playerChoice === "scissors" && cpuChoice === "rock")) {
+    (playerChoice === "paper" && cpuChoice === "scissors") || (playerChoice === "scissors" && cpuChoice === "rock")) {
         cpuScore++;
-        return losingMessage;
+        return "You lost this round!";
     } else if ((playerChoice === "rock" && cpuChoice === "scissors") || 
-    (playerChoice === "paper" && cpuChoice === "rock") || 
-    (playerChoice === "scissors" && cpuChoice === "paper")) {
+    (playerChoice === "paper" && cpuChoice === "rock") || (playerChoice === "scissors" && cpuChoice === "paper")) {
         playerScore++;
-        return winningMessage;
-    } else return "That's not a valid input!";
-}
-
-function initDisplay() {
-    const display = document.getElementById("display");
-    const playerDiv = document.createElement("div");
-    const cpuDiv = document.createElement("div");
+        return "You won this round!";
+    }
 }
 
 function startUp() {
-    const choice = document.getElementById("choice");
+    // init vars
+    playerScore = 0;
+    cpuScore = 0;
+
+    const message = document.getElementById("message");
     const para = document.createElement('p');
+    const rock = document.getElementById("rock-btn");
+    const paper = document.getElementById("paper-btn");
+    const scissors = document.getElementById("scissors-btn");
+
     para.textContent = "Welcome to Rock Paper Scissors! Play against a computer by clicking on any of the button below. First to 5 win!";
-    choice.appendChild(para);
+    message.appendChild(para);
+
+    updateScoreBoard();
+
+    rock.addEventListener('click', () => {handleClick('rock')});
+    paper.addEventListener('click', () => {console.log("PAPER")});
+    scissors.addEventListener('click', () => {console.log("SCISSORS")});
 }
 
-function game() {
-    let i = 0;
-    cpuScore = 0;
-    playerScore = 0;
-    while (i != 5) {
-        playerChoice = getPlayerChoice();
-        cpuChoice = getComputerChoice();
-        console.log(playRound(playerChoice, cpuChoice));
-        i++;
-        console.log("Current score: You: " + playerScore + " , CPU: " + cpuScore);
-        if (cpuScore === 3 || playerScore === 3) break;
+function updateScoreBoard() {
+    const player = document.querySelector("#playerScore");
+    player.textContent = "Player Score: " + playerScore;
+    const cpu = document.querySelector("#cpuScore");
+    cpu.textContent = "Computer Score: " + cpuScore;
+}
+
+function handleClick(playerChoice) {
+    const message = document.querySelector("#message p");
+    cpuChoice = getComputerChoice();
+    message.textContent = "You played " + playerChoice + " and the CPU played " + cpuChoice + ". "
+    message.textContent = message.textContent + playRound(playerChoice, cpuChoice);
+    updateScoreBoard();
+
+    if (playerScore === 5 || cpuScore === 5) {
+        handleGameOver();
     }
-    (playerScore > cpuScore) ? alert("Congratulations, you win!") 
-    : ((playerScore < cpuScore) ? alert("Unfortunately, you lost!") 
-    : alert("It's a draw!"));
+}
+
+function handleGameOver() {
+    const restartBtn = document.createElement("button");
+    restartBtn.textContent = "Restart";
+    if (playerScore === 5) {
+        message.textContent = message.textContent + " You win! Click the restart button to try again!";
+    }
+    const container = document.querySelector("#container");
+    container.appendChild(restartBtn);
+    document.getElementById("rock-btn").disabled = true;
+    document.getElementById("paper-btn").disabled = true;
+    document.getElementById("scissors-btn").disabled = true;
 }
 
 startUp();
-// disabling prompt
-// game();
